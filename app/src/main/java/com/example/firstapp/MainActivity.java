4c05e422ab15939be8aa845e98c941938106af8c
manjusha8294 <manjushakumari8882718595@gmail.com>
2023-10-02 21:42:40 +0530

    project_1

---
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+  
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
